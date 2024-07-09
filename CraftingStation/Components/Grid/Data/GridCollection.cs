e2a3b1fd895817c9f35a0113ba915fbd33f1f79d
Jerome <jerome.baur@students.fhnw.ch>
2024-07-09 21:07:36 +0200

    Projektdateien hinzufügen.

---
+++ CraftingStation/Components/Grid/Data/GridCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CraftingStation.Components.Grid.Data
+{
+    public class GridCollection<T> : ObservableCollection<T>
+    {
+        private Func<T, object> selector;
+
+        public GridCollection(Func<T, object> selector)
+        {
+            this.selector = selector;
+        }
+
+
+        public void UpdateElement(T element)
+        {
+            
+           var found = this.FirstOrDefault(e => selector(e).Equals(selector(element)));
+            if (found != null)
+            {
+                int i = this.IndexOf(found);
+                this[i] = element;
+                
+            }
+        }
+
+    }
+}
---
+++ CraftingStation/Components/Grid/Data/SampleClass.cs
@@ -0,0 +1,12 @@
+﻿namespace CraftingStation.Components.Grid.Data
+{
+    public class SampleClass
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public int SomeNumber { get; set; } 
+        public string Uneditable { get; set; }
+        public bool RandomBool { get; set; }
+
+    }
+}
